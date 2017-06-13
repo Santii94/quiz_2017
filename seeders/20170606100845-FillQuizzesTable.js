6f48ffccbe325ddefe6ac10e2b01d49414dc0cdd
Santii94 <santiago.salamanca.garcia@alumnos.upm.es>
2017-06-13 13:38:08 +0200

    practica53

--- seeders/20170606100845-FillQuizzesTable.js
+++ seeders/20170606100845-FillQuizzesTable.js
@@ -1,26 +1,38 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: function (queryInterface, Sequelize) {
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+        return queryInterface.bulkInsert('Quizzes', [
+            {
+                question: 'Capital de Italia',
+                answer: 'Roma',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                question: 'Capital de Portugal',
+                answer: 'Lisboa',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                question: 'Capital de España',
+                answer: 'Madrid',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                question: 'Capital de Francia',
+                answer: 'París',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]);
+    },
 
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: function (queryInterface, Sequelize) {
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
+        return queryInterface.bulkDelete('Quizzes', null, {});
+    }
 };
